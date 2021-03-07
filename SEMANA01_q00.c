2446cfe430578ed53dbbfea1f00051e676f03ce9
Misael Machado <misaelmachado22@acad.charqueadas.ifsul.edu.br>
2021-03-07 23:11:14 +0000

    Initial commit

---
+++ SEMANA01_q00.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int n1, n2;
+  printf("Digite o primeiro numero\n");
+  scanf("%i",&n1);
+  printf("Digite o segundo numero\n");
+  scanf("%i",&n2);
+  if(n1<=n2){
+    printf ("%i %i",n1, n2);
+    
+  }
+  else
+    printf ("%i %i",n2, n1);
+
+  return 0;
+}
